0722e5559faedcaf08a66f2ac4583a0cf83bb833
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-06 16:41:45 +0200

    feat: introduce Kcipt

---
+++ modules/core/kotlin/com/github/timaxinc/kcipt/Kcipt.kt
@@ -0,0 +1,8 @@
+package com.github.timaxinc.kcipt
+
+import com.github.timaxinc.kcipt.compile.Compiler
+import com.github.timaxinc.kcipt.execute.Executor
+
+class Kcipt(val compiler: Compiler, val executor: Executor) {
+
+}
