0ccfc3a1b697facaa23bd2526c9030747ed8d489
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-06 17:46:47 +0200

    feat: introduce basic source providers

---
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/provider/FileSource.kt
@@ -0,0 +1,21 @@
+package com.github.timaxinc.kcipt.source.provider
+
+import com.github.timaxinc.kcipt.source.MutableSource
+import com.github.timaxinc.kcipt.util.io.setupAsMutableFile
+import java.io.File
+
+class FileSource(private val file: File) : MutableSource<String> {
+
+    constructor(path: String) : this(File(path))
+
+    override fun write(t: String) {
+        if (file.setupAsMutableFile())
+            file.writeText(t)
+    }
+
+    override fun read(): String {
+        if (file.setupAsMutableFile())
+            return file.readText()
+        return ""
+    }
+}
