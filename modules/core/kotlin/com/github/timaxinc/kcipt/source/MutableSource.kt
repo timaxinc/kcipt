f3c718ee05e840d7c7c59de962af41aefa63349c
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-06 17:43:18 +0200

    feat: introduce Sources

---
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/MutableSource.kt
@@ -0,0 +1,5 @@
+package com.github.timaxinc.kcipt.source
+
+interface MutableSource<T> : Source<T> {
+    fun write(t: T)
+}
---
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/Source.kt
@@ -0,0 +1,5 @@
+package com.github.timaxinc.kcipt.source
+
+interface Source<T> {
+    fun read(): T
+}
