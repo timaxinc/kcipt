8bf7ef050e57dc8a72d92f80192a0dba8a56b882
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-07 02:21:31 +0200

    feat: add abstract compile and execute methods

--- modules/core/kotlin/com/github/timaxinc/kcipt/Kcipt.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/Kcipt.kt
@@ -5,4 +5,7 @@ import com.github.timaxinc.kcipt.execute.Executor
 
 class Kcipt(val compiler: Compiler, val executor: Executor) {
 
+    fun compile(script: Script): CompiledScript = TODO("implement compiler delegate")
+
+    fun execute(compiledScript: CompiledScript): CompiledScript = TODO("implement executor delegate")
 }
