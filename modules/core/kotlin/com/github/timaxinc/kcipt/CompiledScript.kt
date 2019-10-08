7290f8b6d6969b110cd63d5fd5ae19a919fb7edd
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-08 19:47:18 +0200

    fix: unnecessary member

--- modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
@@ -1,3 +1,3 @@
 package com.github.timaxinc.kcipt
 
-open class CompiledScript(val source: Script, val rawClasses: Map<String, ByteArray>) : Script by source
+open class CompiledScript(source: Script, val rawClasses: Map<String, ByteArray>) : Script by source
