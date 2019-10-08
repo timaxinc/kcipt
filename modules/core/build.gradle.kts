a0f059937b01fe8bb389ef3ce9df941e92ade947
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-08 03:58:23 +0200

    fix: gradle dependency

--- modules/core/build.gradle.kts
+++ modules/core/build.gradle.kts
@@ -2,6 +2,7 @@ normalGradleProject()
 
 dependencies {
     compile(project(":util"))
+    compile(project(":lib"))
     compile(Deps.Jvm.kotlinScriptCommon)
     compile(Deps.Jvm.kotlinScriptJvm)
     compile(Deps.Jvm.kotlinScriptJvmHost)
