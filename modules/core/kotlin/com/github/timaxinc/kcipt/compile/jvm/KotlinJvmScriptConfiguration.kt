77ac59820bb0ba7f7d5995a0a8cd6b842dff818b
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-10 01:36:36 +0200

    style: code cleanup

--- modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmScriptConfiguration.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmScriptConfiguration.kt
@@ -1,7 +1,9 @@
 package com.github.timaxinc.kcipt.compile.jvm
 
 import com.github.timaxinc.kcipt.ScriptConfiguration
-import com.github.timaxinc.kcipt.delegate
+import com.github.timaxinc.kcipt.util.collections.delegate
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
 
-var ScriptConfiguration.kotlinJvmScriptCompilationConfiguration: ScriptCompilationConfiguration by delegate(ScriptCompilationConfiguration {})
+var ScriptConfiguration.kotlinJvmScriptCompilationConfiguration: ScriptCompilationConfiguration by delegate(
+        ScriptCompilationConfiguration {}
+)
