5f6ecd8eeddfd25d02bb59d6612ce30443303321
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-10 01:40:09 +0200

    refactor: rename rawClasses to jvmRawClasses and move it into extension property

--- modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
@@ -1,3 +1,3 @@
 package com.github.timaxinc.kcipt
 
-open class CompiledScript(source: Script, val rawClasses: Map<String, ByteArray>) : Script by source
+open class CompiledScript(source: Script) : Script by source, MutableMap<String, Any> by mutableMapOf()
--- modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmCompiledScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmCompiledScript.kt
@@ -2,17 +2,22 @@ package com.github.timaxinc.kcipt.compile.jvm
 
 import com.github.timaxinc.kcipt.CompiledScript
 import com.github.timaxinc.kcipt.Script
+import com.github.timaxinc.kcipt.util.collections.delegate
 import org.jetbrains.kotlin.scripting.compiler.plugin.impl.KJvmCompiledModuleInMemory
 import kotlin.script.experimental.jvm.impl.KJvmCompiledScript
 import kotlin.script.experimental.api.CompiledScript as KotlinCompiledScript
 
+var CompiledScript.jvmRawClasses: Map<String, ByteArray> by delegate(emptyMap())
+
 class KotlinJvmCompiledScript(
         val compiledScript: KotlinCompiledScript<*>,
         script: Script
-) : CompiledScript(
-        script,
-        compiledScript.rawClasses
-) {
+) : CompiledScript(script) {
+
+    init {
+        jvmRawClasses = compiledScript.rawClasses
+    }
+
     companion object {
         internal val KotlinCompiledScript<*>.rawClasses: Map<String, ByteArray>
             get() = readRawClassesFromCompiledScript()
