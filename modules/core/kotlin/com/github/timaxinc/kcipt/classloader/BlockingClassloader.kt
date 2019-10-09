37ef60b6b77f21a5e85e99141a44d8cf45a139ad
mavolin <48887425+mavolin@users.noreply.github.com>
2019-10-09 22:59:37 +0200

    feat: BlockingClassloader

--- modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlockingClassloader.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlockingClassloader.kt
@@ -1,22 +1,94 @@
 package com.github.timaxinc.kcipt.classloader
 
-class BlockingClassloader(private val parentLoader: ClassLoader, private val blocked: MutableList<String>) : ClassLoader() {
+import java.net.URL
+import java.util.*
 
-    constructor(parentLoader: ClassLoader, vararg blocked: String) : this(parentLoader, blocked.toMutableList())
+/**
+ * BlockingClassLoader is a ClassLoader holding blacklist of packages and classes, as well as a parent ClassLoader.
+ * All classes and packages contained in that List may not be used when attempting to load a Class or Resource
+ *
+ * @property parentLoader
+ *          the ClassLoader that will be used, if the requested class passes the blacklist check
+ * @property blacklist
+ *          the blacklist containing names of Classes, as well as of packages, which may not be used when loading a
+ *          class or resource.
+ */
+class BlockingClassloader(private val parentLoader: ClassLoader, private val blacklist: List<String>) :
+        ClassLoader(parentLoader) {
 
-    fun add(toBlock: String) = blocked.add(toBlock)
-    fun remove(toRemove: String) = blocked.remove(toRemove)
+    constructor(parentLoader: ClassLoader, vararg blocked: String) : this(parentLoader, blocked.toList())
 
+    /**
+     * LoadClass loads the class with the specified name. If the class or its package is present in the blacklist,
+     * loadClass will throw an Exception
+     *
+     * @param name
+     *          the name of the class to be loaded
+     * @return
+     *          the loaded class
+     *
+     * @throws ClassBlockedException
+     *          In case the requested class is blocked.
+     * @throws PackageBlockedException
+     *          in case the package containing the requested class is blocked.
+     */
     override fun loadClass(name: String?): Class<*> {
+        blockCheck(name)
+        return super.loadClass(name)
+    }
+
+    /**
+     * GetResource will get the first resource matched by the passed name. If the class or its package is present in the
+     * blacklist, loadClass will throw an Exception.
+     *
+     * @param name
+     *          the name of the class whose resource is to be returned
+     * @return
+     *          the resource
+     *
+     * @throws ClassBlockedException
+     *          In case the requested class is blocked.
+     * @throws PackageBlockedException
+     *          in case the package containing the requested class is blocked.
+     */
+    override fun getResource(name: String?): URL? {
+        blockCheck(name)
+        return super.getResource(name)
+    }
+
+    /**
+     * GetResources gets all resources matching the passed name. f the class or its package is present in the blacklist,
+     * loadClass will throw an Exception.
+     *
+     * @param name
+     *          the name of the class whose resources are to be returned
+     * @return
+     *          the resources
+     *
+     * @throws ClassBlockedException
+     *          In case the requested class is blocked.
+     * @throws PackageBlockedException
+     *          in case the package containing the requested class is blocked.
+     */
+    override fun getResources(name: String?): Enumeration<URL> {
+        blockCheck(name)
+        return super.getResources(name)
+    }
+
+    /**
+     * Checks if the name of the class is on the blacklist of classes and packages. If that is the case, the
+     * corresponding exception will be thrown.
+     *
+     * @param name
+     *          the name of the class
+     */
+    private fun blockCheck(name: String?) {
         if (name==null) {
-            return parentLoader.loadClass(name)
+            return
         }
 
-
-        when(val it = name startsWithMember blocked) {
-            is Block.None-> {
-                return parentLoader.loadClass(name)
-            }
+        when (val it = name startsWithMember blacklist) {
+            is Block.None -> return
             is Block.Class -> throw ClassBlockedException(it.className)
             is Block.Package -> throw PackageBlockedException(it.packageName)
         }
@@ -25,15 +97,17 @@ class BlockingClassloader(private val parentLoader: ClassLoader, private val blo
     /**
      * Checks uf there if the passed element starts with any of the members of the MutableList.
      *
-     * @param list the List with the {@link String Strings} which shall be used for the check
+     * @param list
+     *          the List with the String which shall be used for the check
      *
-     * @return {@code true} if the String starts with at least one of the members of the members of the List; {@code
-     *         false} otherwise
+     * @return
+     *          {@code true} if the String starts with at least one of the members of the members of the List; {@code
+     *          false} otherwise
      */
     private infix fun String.startsWithMember(list: List<String>): Block {
         list.forEach {
             if (this.startsWith(it)) {
-                if (this == it) return Block.Class(it)
+                if (this==it) return Block.Class(it)
                 return Block.Package(it)
             }
         }
@@ -46,7 +120,35 @@ class BlockingClassloader(private val parentLoader: ClassLoader, private val blo
         class Package(val packageName: String) : Block()
     }
 
+    /**
+     * Abstract representation of an exception thrown, if the blacklist has prevented the requested operation.
+     *
+     * @constructor
+     *          Instantiates a new BlockedException.
+     *
+     * @param message
+     *          The message of the exception
+     */
     abstract class BlockedException internal constructor(message: String) : Exception(message)
+
+    /**
+     * ClassBlockedException gets thrown in case the specified class is present on the blacklist.
+     *
+     * @constructor
+     *          Instantiates a new ClassBlockedException.
+     *
+     * @param name
+     *          The name of the Class being blocked.
+     */
     class ClassBlockedException internal constructor(name: String) : BlockedException("Class $name is blocked")
+    /**
+     * PackageBlockedException gets thrown in case the package containing the class is present on the blacklist.
+     *
+     * @constructor
+     *          Instantiates a new PackageBlockedException.
+     *
+     * @param name
+     *          The name of the package causing the class to be blocked.
+     */
     class PackageBlockedException internal constructor(name: String) : BlockedException("Package $name is blocked")
 }
