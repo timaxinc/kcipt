6c185e9785ae2d2988ecb40cd3d82d929cba8aef
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 00:47:53 +0200

    docs: KotlinJvmScriptCompilerReport

--- modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmScriptCompilerReport.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmScriptCompilerReport.kt
@@ -3,4 +3,9 @@ package com.github.timaxinc.kcipt.compile.jvm
 import com.github.timaxinc.kcipt.compile.CompilerReport
 import kotlin.script.experimental.api.ScriptDiagnostic
 
+/**
+ * Bridge for [ScriptDiagnostic]
+ *
+ * @property diagnostic [ScriptDiagnostic] from kotlin scripting api
+ */
 class KotlinJvmScriptCompilerReport(val diagnostic: ScriptDiagnostic) : CompilerReport(diagnostic.message)
