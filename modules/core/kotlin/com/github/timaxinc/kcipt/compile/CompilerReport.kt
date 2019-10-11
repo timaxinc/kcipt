2b3e6a8a830edc9cac04c0acd7984714ea3a16c8
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 01:33:00 +0200

    docs: CompilerReport

--- modules/core/kotlin/com/github/timaxinc/kcipt/compile/CompilerReport.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/compile/CompilerReport.kt
@@ -1,3 +1,8 @@
 package com.github.timaxinc.kcipt.compile
 
+/**
+ * can be returned by a [Compiler] in case of a compilation failure
+ *
+ * @property message
+ */
 open class CompilerReport(val message: String)
