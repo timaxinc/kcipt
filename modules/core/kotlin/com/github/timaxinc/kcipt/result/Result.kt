d9665d15df01d0058ad623becef27161ca9ab1b6
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 01:58:20 +0200

    docs: Result

--- modules/core/kotlin/com/github/timaxinc/kcipt/result/Result.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/result/Result.kt
@@ -1,9 +1,26 @@
 package com.github.timaxinc.kcipt.result
 
+/**
+ * represents a result of a process that can succeed or fail
+ *
+ * @param V type of value in case result is [Result.Success]
+ * @param R type of [reports]
+ */
 sealed class Result<V, R> {
 
+    /**
+     * list of reports that were created by the process
+     */
     abstract val reports: List<R>
 
+    /**
+     * returned by a process in case it succeeds
+     *
+     * @param V type of [value]
+     * @param R type of [reports]
+     * @property value result of the process
+     * @property reports list of reports that were created by the process
+     */
     data class Success<V, R>(
             val value: V, override val reports: List<R> = listOf()
     ) : Result<V, R>() {
@@ -11,6 +28,13 @@ sealed class Result<V, R> {
         constructor(value: V, vararg reports: R) : this(value, reports.asList())
     }
 
+    /**
+     * returned by a process in case it fails
+     *
+     * @param V type of value in case result is [Result.Success]
+     * @param R type of [reports]
+     * @property reports list of reports that were created by the process
+     */
     data class Failure<V, R>(
             override val reports: List<R>
     ) : Result<V, R>() {
