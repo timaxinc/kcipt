7b5e9d4ea3785ed47c158588bc23c57eb49e39dd
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-11 23:41:40 +0200

    docs: KotlinJvmCompiledScript

--- modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmCompiledScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/compile/jvm/KotlinJvmCompiledScript.kt
@@ -7,30 +7,42 @@ import org.jetbrains.kotlin.scripting.compiler.plugin.impl.KJvmCompiledModuleInM
 import kotlin.script.experimental.jvm.impl.KJvmCompiledScript
 import kotlin.script.experimental.api.CompiledScript as KotlinCompiledScript
 
+/**
+ * Used to store classes in raw bytecode in a map. Key is the full class name. Value is the bytecode.
+ */
 var CompiledScript.jvmRawClasses: Map<String, ByteArray> by delegate { mapOf<String, ByteArray>() }
 
+/**
+ * [KotlinJvmCompiledScript] describes a kotlin script that was compiled on and for jvm.
+ * Is a used as a bridge to [KotlinCompiledScript].
+ *
+ * @property kotlinJvmCompiledScript the CompiledScript from the Jetbrains kotlin script compiler
+ *
+ * @constructor
+ * @param script the source script that was compiled
+ */
 class KotlinJvmCompiledScript(
         @Suppress("UNUSED")
         val kotlinJvmCompiledScript: KotlinCompiledScript<*>, script: Script
 ) : CompiledScript(script) {
 
     init {
-        jvmRawClasses = kotlinJvmCompiledScript.rawClasses
+        jvmRawClasses = kotlinJvmCompiledScript.readRawClassesFromCompiledScript()
     }
 
-    companion object {
-        internal val KotlinCompiledScript<*>.rawClasses: Map<String, ByteArray>
-            get() = readRawClassesFromCompiledScript()
+    /**
+     * reads raw classes from a [KotlinCompiledScript]
+     *
+     * @return a map of full class name and bytecode
+     */
+    private fun KotlinCompiledScript<*>.readRawClassesFromCompiledScript(): Map<String, ByteArray> {
+        val jvmCompiledScript = this as KJvmCompiledScript
+        val jvmCompiledModule = jvmCompiledScript.compiledModule as KJvmCompiledModuleInMemory
 
-        private fun KotlinCompiledScript<*>.readRawClassesFromCompiledScript(): Map<String, ByteArray> {
-            val jvmCompiledScript = this as KJvmCompiledScript
-            val jvmCompiledModule = jvmCompiledScript.compiledModule as KJvmCompiledModuleInMemory
+        val compilerOutputFiles = jvmCompiledModule.compilerOutputFiles
 
-            val compilerOutputFiles = jvmCompiledModule.compilerOutputFiles
-
-            return compilerOutputFiles.filterKeys { it.endsWith(".class") }.map {
-                it.key.removeSuffix(".class").replace("/", ".").replace("\\", ".") to it.value
-            }.toMap()
-        }
+        return compilerOutputFiles.filterKeys { it.endsWith(".class") }.map {
+            it.key.removeSuffix(".class").replace("/", ".").replace("\\", ".") to it.value
+        }.toMap()
     }
 }
