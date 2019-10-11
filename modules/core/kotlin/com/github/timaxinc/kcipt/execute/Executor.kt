0d1d4792dab234e45b887454ad11a1a412573528
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 01:34:25 +0200

    docs: Executor

--- modules/core/kotlin/com/github/timaxinc/kcipt/execute/Executor.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/execute/Executor.kt
@@ -2,6 +2,10 @@ package com.github.timaxinc.kcipt.execute
 
 import com.github.timaxinc.kcipt.CompiledScript
 
+/**
+ * should be implemented by every kcipt script executor implementation
+ */
 interface Executor {
+
     operator fun invoke(compiledScript: CompiledScript)
 }
