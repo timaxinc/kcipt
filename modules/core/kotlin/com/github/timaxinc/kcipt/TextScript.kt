e02f747375b86406066fc7da310101269fce9fc5
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 04:05:14 +0200

    docs: TextScript

--- modules/core/kotlin/com/github/timaxinc/kcipt/TextScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/TextScript.kt
@@ -1,3 +1,9 @@
 package com.github.timaxinc.kcipt
 
+/**
+ * implementation of [Script] that gets sourcecode from [String] [text]
+ *
+ * @property text sourcecode of [Script]
+ * @property configuration [ScriptConfiguration] of [Script]
+ */
 class TextScript(override val text: String, override val configuration: ScriptConfiguration) : Script
