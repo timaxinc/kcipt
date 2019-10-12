edb94e8d3f88cfaf40a7295d4678f418fb0014a3
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 02:57:21 +0200

    docs: FileSource

--- modules/core/kotlin/com/github/timaxinc/kcipt/source/provider/FileSource.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/provider/FileSource.kt
@@ -4,14 +4,29 @@ import com.github.timaxinc.kcipt.source.MutableSource
 import com.github.timaxinc.kcipt.util.io.setupAsMutableFile
 import java.io.File
 
+/**
+ * a [MutableSource] that wraps a given [File]
+ *
+ * @property file the wrapped [File]
+ */
 class FileSource(private val file: File) : MutableSource<String> {
 
     constructor(path: String) : this(File(path))
 
+    /**
+     * tris to write [t] in [File] [file]
+     *
+     * @param t the [String] to write
+     */
     override fun write(t: String) {
         if (file.setupAsMutableFile()) file.writeText(t)
     }
 
+    /**
+     * tris to read content of [File] [file]
+     *
+     * @return the content of [File] [file] or a empty [String]
+     */
     override fun read(): String {
         if (file.setupAsMutableFile()) return file.readText()
         return ""
