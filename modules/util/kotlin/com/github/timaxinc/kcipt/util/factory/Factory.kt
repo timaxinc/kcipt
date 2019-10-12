1591583bab63b3b237857dbe62fa9c39d68bf0b7
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 05:13:53 +0200

    docs: Factory

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/Factory.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/Factory.kt
@@ -1,5 +1,16 @@
 package com.github.timaxinc.kcipt.util.factory
 
+/**
+ * creates a object with type [T]
+ *
+ * @param T type of the created object
+ */
 interface Factory<T> {
+
+    /**
+     * creates a object with type [T]
+     *
+     * @return created object with type [T]
+     */
     operator fun invoke(): T
 }
