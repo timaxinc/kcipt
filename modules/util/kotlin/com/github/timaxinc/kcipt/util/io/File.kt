28c5f55c4e602485f561d3d82c0c829f39e04fcc
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 05:46:30 +0200

    docs: File utilities

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/io/File.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/io/File.kt
@@ -1,23 +1,45 @@
 package com.github.timaxinc.kcipt.util.io
 
 import java.io.File
+import java.io.IOException
 
+/**
+ * tris to create the [File] if it doesn't exists
+ *
+ * @return if [File] exists after creation
+ */
 fun File.createFileIfNotExists(): Boolean {
-    if (!exists()) {
-        parentFile?.mkdirs()
-        createNewFile()
+    try {
+        if (!exists()) {
+            parentFile?.mkdirs()
+            createNewFile()
+        }
+    } catch (e: IOException) {
     }
     return exists()
 }
 
+/**
+ * tris to create the [File] as Folder if it doesn't exists
+ *
+ * @return if folder exists after creation
+ */
 fun File.createFolderIfNotExists(): Boolean {
-    if (!exists()) {
-        parentFile?.mkdirs()
-        mkdir()
+    try {
+        if (!exists()) {
+            parentFile?.mkdirs()
+            mkdir()
+        }
+    } catch (e: IOException) {
     }
     return exists()
 }
 
+/**
+ * tris to setup a file as read and writeable
+ *
+ * @return if setup was successful
+ */
 fun File.setupAsMutableFile(): Boolean {
     if (isFile && canRead() && canWrite()) {
         return createFileIfNotExists()
@@ -25,7 +47,12 @@ fun File.setupAsMutableFile(): Boolean {
     return false
 }
 
-fun File.splintedFilePath(): SplintedFilePath {
+/**
+ * splits [File] path
+ *
+ * @return [SplintedFilePath] that contains the splinted version of [File] path
+ */
+fun File.splintFilePath(): SplintedFilePath {
     var fullPath = absolutePath
     fullPath = fullPath.replace("\\", "/")
     val directory = fullPath.substringBeforeLast("/")
@@ -35,7 +62,18 @@ fun File.splintedFilePath(): SplintedFilePath {
     return SplintedFilePath(directory, fullName, fileName, extension)
 }
 
+/**
+ * the [SplintedFilePath] that contains the splinted version of [File] path
+ */
 val File.splintedFilePath: SplintedFilePath
-    get() = splintedFilePath()
+    get() = splintFilePath()
 
+/**
+ * represents a file path in a splinted form
+ *
+ * @property directory the directory the [File] is located in
+ * @property fullName the full name of the [File]
+ * @property fileName the name of [File]
+ * @property extension the [File] extension
+ */
 data class SplintedFilePath(var directory: String, var fullName: String, var fileName: String, var extension: String)
