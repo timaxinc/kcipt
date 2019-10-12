4f028c51376f6c7851c0fc0016c0ecdb72e7f092
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 13:38:56 +0200

    test: add ResultTest

---
+++ modules/test/test/com/github/timaxinc/kcipt/result/ResultTest.kt
@@ -0,0 +1,49 @@
+package com.github.timaxinc.kcipt.result
+
+import com.github.timaxinc.kcipt.test.notReachable
+import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.AnnotationSpec
+
+class ResultTest : AnnotationSpec() {
+
+    @Test
+    fun `simple successful result`() {
+        createSuccess<DummyValue, DummyReport>(DummyValue).onFailure {
+            notReachable()
+        }.onSuccess {
+            value shouldBe DummyValue
+            return
+        }
+        notReachable()
+    }
+
+    @Test
+    fun `simple successful result with report`() {
+        val dummyReport = DummyReport("unicorn")
+        createSuccess<DummyValue, DummyReport>(DummyValue, listOf(dummyReport)).onFailure {
+            notReachable()
+        }.onSuccess {
+            value shouldBe DummyValue
+            reports shouldContain dummyReport
+            return
+        }
+        notReachable()
+    }
+
+    @Test
+    fun `simple failed result with report`() {
+        val dummyReport = DummyReport("unicorn")
+        createFailure<DummyValue, DummyReport>(listOf(dummyReport)).onFailure {
+            reports shouldContain dummyReport
+            return
+        }.onSuccess {
+            notReachable()
+        }
+        notReachable()
+    }
+}
+
+private object DummyValue
+
+private class DummyReport(val message: String)
