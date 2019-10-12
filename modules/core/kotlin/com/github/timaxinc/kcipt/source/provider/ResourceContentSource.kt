b6069936a1b259065c75baa5aa47598bca1c43f2
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 03:15:07 +0200

    docs: ResourceContentSource

--- modules/core/kotlin/com/github/timaxinc/kcipt/source/provider/ResourceContentSource.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/provider/ResourceContentSource.kt
@@ -3,7 +3,18 @@ package com.github.timaxinc.kcipt.source.provider
 import com.github.timaxinc.kcipt.source.Source
 import com.github.timaxinc.kcipt.util.io.Resource
 
+/**
+ * a [Source] that wraps a [Resource] with a given path
+ *
+ * @property resourcePath
+ */
 class ResourceContentSource(private val resourcePath: String) : Source<String> {
+
+    /**
+     * tris to read content of [Resource] with path [resourcePath]
+     *
+     * @return the content of the [Resource]
+     */
     override fun read(): String {
         return Resource(resourcePath).readText()
     }
