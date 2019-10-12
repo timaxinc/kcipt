74c8e2ba49aa4d5ce61878e5272e1c48bc103b9a
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 06:07:41 +0200

    docs: Resource utilities

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/io/Resource.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/io/Resource.kt
@@ -2,18 +2,48 @@ package com.github.timaxinc.kcipt.util.io
 
 import kotlin.reflect.KClass
 
+/**
+ * wrapper used to work with resources more easily
+ *
+ * @constructor
+ * @param path the path of the recourse
+ * @param context the context [KClass] of the resource
+ */
 class Resource(path: String, context: KClass<*>? = null) {
-    private val pre: String = if (!path.startsWith("/")) "/" else ""
 
-    val url =
-            (context
-             ?: this::class).java.getResource(pre + path)
+    /**
+     * the context [KClass] of the resource
+     */
+    val context = context ?: this::class
+
+    /**
+     * the path of the recourse
+     */
+    val path: String = (if (!path.startsWith("/")) "/" else "") + path
+
+    /**
+     * resource as URL
+     */
+    val url = this.context.java.getResource(this.path)
+
+    /**
+     * resource as URI
+     */
     val uri = url?.toURI()
-    var externalForm = url?.toExternalForm()
-    var stream =
-            (context
-             ?: this::class).java.getResourceAsStream(pre + path)
 
+    /**
+     * resource as external form
+     */
+    var externalForm = url?.toExternalForm()
+
+    /**
+     * resource as InputStream
+     */
+    var stream = this.context.java.getResourceAsStream(this.path)
+
+    /**
+     * reads the content of the Resource
+     * @return content of Resource as [String]
+     */
     fun readText() = url.readText()
 }
-
