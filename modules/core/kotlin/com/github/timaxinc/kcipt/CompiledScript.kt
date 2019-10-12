963457492e6d66870fb3bb3fbc434c36af2341da
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 03:43:57 +0200

    docs: CompiledScript

--- modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/CompiledScript.kt
@@ -1,3 +1,9 @@
 package com.github.timaxinc.kcipt
 
+/**
+ * representation of a successfully compiled [Script]
+ *
+ * @constructor
+ * @param source the successfully compiled [Script]
+ */
 open class CompiledScript(source: Script) : Script by source, MutableMap<String, Any> by mutableMapOf()
