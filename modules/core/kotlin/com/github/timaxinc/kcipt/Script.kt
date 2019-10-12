8f3d694c0e4adcd4e3a4c24ff091ecf2495d55a3
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 03:54:25 +0200

    docs: Script

--- modules/core/kotlin/com/github/timaxinc/kcipt/Script.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/Script.kt
@@ -1,6 +1,13 @@
 package com.github.timaxinc.kcipt
 
+/**
+ * representation of a script
+ *
+ * @property text the script text
+ * @property configuration configures script compilation and execution
+ */
 interface Script {
+
     val text: String
     val configuration: ScriptConfiguration
 }
