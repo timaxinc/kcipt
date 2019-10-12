1c237e8fcdf32c8024d19ef543fb4e03c16a2086
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 05:11:34 +0200

    docs: DynamicLambdaFactory

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/DynamicLambdaFactory.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/DynamicLambdaFactory.kt
@@ -1,5 +1,17 @@
 package com.github.timaxinc.kcipt.util.factory
 
+/**
+ * implementation of [DynamicFactory] that uses a lambda [block] to create a object
+ *
+ * @property block the lambda used to create the object
+ */
 class DynamicLambdaFactory<P, T>(private val block: (P) -> T) : DynamicFactory<P, T> {
+
+    /**
+     * creates a object with type [T] based on a given [parameter] with type [P] by calling [block]
+     *
+     * @param parameter
+     * @return created object with type [T]
+     */
     override fun invoke(parameter: P) = block(parameter)
 }
