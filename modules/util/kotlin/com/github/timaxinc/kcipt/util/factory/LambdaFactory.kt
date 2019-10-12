633c7fb054689d28db2c9ff1f4130a9235aa0471
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 05:15:25 +0200

    docs: LambdaFactory

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/LambdaFactory.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/factory/LambdaFactory.kt
@@ -1,5 +1,16 @@
 package com.github.timaxinc.kcipt.util.factory
 
+/**
+ * implementation of [Factory] that uses a lambda [block] to create a object
+ *
+ * @property block the lambda used to create the object
+ */
 class LambdaFactory<T>(private val block: () -> T) : Factory<T> {
+
+    /**
+     * creates a object with type [T] by calling [block]
+     *
+     * @return created object with type [T]
+     */
     override fun invoke() = block()
 }
