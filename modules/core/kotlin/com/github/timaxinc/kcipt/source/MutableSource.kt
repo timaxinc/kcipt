67748d5883bf6236c592674fc1c92c16f0ba34f4
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 03:24:03 +0200

    docs: MutableSource

--- modules/core/kotlin/com/github/timaxinc/kcipt/source/MutableSource.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/source/MutableSource.kt
@@ -1,5 +1,17 @@
 package com.github.timaxinc.kcipt.source
 
+/**
+ * [Source] with ability to read and write
+ *
+ * @param T
+ */
 interface MutableSource<T> : Source<T> {
+
+    /**
+     * writes [t]
+     * after call [read] should return a object equal to the written one
+     *
+     * @param t object to write
+     */
     fun write(t: T)
 }
