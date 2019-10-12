57dc4a71ab83e18cad2c07286b6217cbd9d472f1
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 03:56:49 +0200

    docs: ScriptConfiguration

--- modules/core/kotlin/com/github/timaxinc/kcipt/ScriptConfiguration.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/ScriptConfiguration.kt
@@ -1,3 +1,8 @@
 package com.github.timaxinc.kcipt
 
+/**
+ * configures [Script] compilation and execution
+ *
+ * @property impl the map that stores configuration
+ */
 open class ScriptConfiguration(private val impl: MutableMap<String, Any> = mutableMapOf()) : MutableMap<String, Any> by impl
