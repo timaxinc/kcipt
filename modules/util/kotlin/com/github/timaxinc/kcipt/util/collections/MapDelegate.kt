95ae1a10dee0fac42fc18e811ec7e0b0809b081e
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-12 04:56:27 +0200

    docs: MapDelegate

--- modules/util/kotlin/com/github/timaxinc/kcipt/util/collections/MapDelegate.kt
+++ modules/util/kotlin/com/github/timaxinc/kcipt/util/collections/MapDelegate.kt
@@ -4,22 +4,45 @@ import com.github.timaxinc.kcipt.util.factory.Factory
 import com.github.timaxinc.kcipt.util.factory.factory
 import kotlin.reflect.KProperty
 
-fun <V : Any> delegate(defaultValueFactoryBlock: () -> V): MutableMapDelegate<V> =
-        MutableMapDelegate(factory(defaultValueFactoryBlock))
+/**
+ * creates a [MutableMapDelegate]
+ *
+ * @param V type of [MutableMapDelegate] value
+ * @param defaultValueFactoryBlock lambda that creates the default value of the created [MutableMapDelegate]
+ * @return the created [MutableMapDelegate]
+ */
+fun <V : Any> delegate(defaultValueFactoryBlock: () -> V): MutableMapDelegate<V> = MutableMapDelegate(factory(defaultValueFactoryBlock))
 
-fun <V : Any> delegate(defaultValueFactory: Factory<V>? = null): MutableMapDelegate<V> =
-        MutableMapDelegate(defaultValueFactory)
+/**
+ * creates a [MutableMapDelegate]
+ *
+ * @param V type of [MutableMapDelegate] value
+ * @param defaultValueFactory [Factory] that creates the default value of the created [MutableMapDelegate]
+ * @return the created [MutableMapDelegate]
+ */
+fun <V : Any> delegate(defaultValueFactory: Factory<V>? = null): MutableMapDelegate<V> = MutableMapDelegate(defaultValueFactory)
 
+/**
+ * used to delegate a property to a [Map] with key type [String]
+ *
+ * @param V the value type
+ * @property defaultValueFactory [Factory] that creates the default value if it isn't prepend in the [Map]
+ * @property storeDefault defines if the default value should be stored in the [Map] if not prepend in [Map]
+ * @property key the key in [Map] if null it uses property's name
+ */
 class MutableMapDelegate<V : Any> internal constructor(
-        private val defaultValueFactory: Factory<V>? = null,
-        private val storeDefault: Boolean = true,
-        private val key: String? = null
+        private val defaultValueFactory: Factory<V>? = null, private val storeDefault: Boolean = true, private val key: String? = null
 ) {
 
+    /**
+     * operator function used to delegate properties
+     *
+     * @param thisRef the [Map] to store in
+     * @param property the delegated [KProperty]
+     * @return the value stored in [Map] or a new default value created by [defaultValueFactory]
+     */
     operator fun getValue(thisRef: Map<String, Any>, property: KProperty<*>): V {
-        val key =
-                key
-                ?: property.name
+        val key = key ?: property.name
         val valueInConfiguration = thisRef[key]
         return run {
             if (valueInConfiguration != null) {
@@ -32,6 +55,13 @@ class MutableMapDelegate<V : Any> internal constructor(
         }.apply { if (this == null) throw NoValueFoundException(key) }!!
     }
 
+    /**
+     * if [defaultValueFactory] is not null and [storeDefault] is true it stores the created default value in the [map]
+     *
+     * @param map the delegate [Map]
+     * @param key the key in [Map]
+     * @return a new default value created by [defaultValueFactory]
+     */
     private fun storeAndGetDefault(map: Map<String, Any>, key: String): V? {
         return if (defaultValueFactory != null) {
             val defaultValue: V? = defaultValueFactory.invoke()
@@ -42,10 +72,22 @@ class MutableMapDelegate<V : Any> internal constructor(
         } else null
     }
 
+    /**
+     * operator function used to delegate properties
+     *
+     * @param thisRef the [Map] to store in
+     * @param property the delegated [KProperty]
+     * @param value
+     */
     operator fun setValue(thisRef: MutableMap<String, Any>, property: KProperty<*>, value: V) {
-        thisRef[key
-                ?: property.name] = value
+        thisRef[key ?: property.name] = value
     }
 
+    /**
+     * is thrown if no value is present in [Map]
+     *
+     * @constructor
+     * @param key the key in [Map]
+     */
     class NoValueFoundException(key: String) : Exception("No value mapped to $key")
 }
