6dd16d4b712fb3d854130c81873911d8d09a1317
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-14 00:25:43 +0200

    test: ResourceSource

--- modules/test/test/com/github/timaxinc/kcipt/source/provider/ResourceSourceTest.kt
+++ modules/test/test/com/github/timaxinc/kcipt/source/provider/ResourceSourceTest.kt
@@ -1,9 +1,18 @@
 package com.github.timaxinc.kcipt.source.provider
 
+import com.github.timaxinc.kcipt.util.io.Resource
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.AnnotationSpec
 
 internal class ResourceSourceTest : AnnotationSpec() {
 
     @Test
-    fun `Fun`
+    fun `FUNCTION - read content of file virtual`() {
+        val resourcePath = ResourceSourceTest::class.qualifiedName?.replace(".", "/") + ".class"
+        val resource = Resource(resourcePath)
+        val resourceContend = resource.readText()
+
+        val resourceSource = ResourceSource(resourcePath)
+        resourceSource.read() shouldBe resourceContend
+    }
 }
