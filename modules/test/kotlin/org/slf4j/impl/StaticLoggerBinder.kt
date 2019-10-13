adebc137522e3f4923e1f7b8be86e6a63863ca0a
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-13 22:46:51 +0200

    style: add newline

--- modules/test/kotlin/org/slf4j/impl/StaticLoggerBinder.kt
+++ modules/test/kotlin/org/slf4j/impl/StaticLoggerBinder.kt
@@ -5,6 +5,7 @@ package org.slf4j.impl
 import org.slf4j.spi.LoggerFactoryBinder
 
 object StaticLoggerBinder : LoggerFactoryBinder by Slf4JLogBinder {
+
     @JvmStatic
     fun getSingleton() = this
 }
