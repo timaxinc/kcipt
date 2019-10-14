5fd872107f6634c136ef1e601b2c48c412c043b4
mavolin <48887425+mavolin@users.noreply.github.com>
2019-10-14 22:52:17 +0200

    test(ByteClassloader): findClass(String)

--- modules/test/test/com/github/timaxinc/kcipt/classloader/ByteClassloaderTest.kt
+++ modules/test/test/com/github/timaxinc/kcipt/classloader/ByteClassloaderTest.kt
@@ -6,10 +6,10 @@ import io.kotlintest.specs.AnnotationSpec
 internal class ByteClassloaderTest : AnnotationSpec() {
     @Test
     fun `FUNCTION findClass(String) - class in class defs`() {
-        val classPath = "/${ByteClassloaderTestDummyClass::class.qualifiedName!!.replace(".", "/")}.class"
+        val classPath = "${ByteClassloaderTestDummyClass::class.qualifiedName!!.replace(".", "/")}.class"
         val dummyBytecode = ByteClassloaderTestDummyClass::class.java.classLoader.getResource(classPath)!!.readBytes()
         val bcl = ByteClassloader(mapOf(ByteClassloaderTestDummyClass::class.qualifiedName!! to dummyBytecode))
-        bcl.loadClass(ByteClassloaderTestDummyClass::class.qualifiedName).kotlin shouldBe ByteClassloaderTestDummyClass::class
+        bcl.loadClass(ByteClassloaderTestDummyClass::class.qualifiedName).kotlin.qualifiedName shouldBe ByteClassloaderTestDummyClass::class.qualifiedName
     }
 }
 
