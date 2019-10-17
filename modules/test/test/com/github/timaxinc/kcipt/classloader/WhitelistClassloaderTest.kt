798482bcf308aea0ed4e9ff8947ff469adc04234
mavolin <48887425+mavolin@users.noreply.github.com>
2019-10-17 21:50:07 +0200

    test(WhitelistClassloader): getResource(String?): URL?

--- modules/test/test/com/github/timaxinc/kcipt/classloader/WhitelistClassloaderTest.kt
+++ modules/test/test/com/github/timaxinc/kcipt/classloader/WhitelistClassloaderTest.kt
@@ -3,6 +3,8 @@ package com.github.timaxinc.kcipt.classloader
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.AnnotationSpec
+import java.net.URL
+import java.util.*
 
 internal class WhitelistClassloaderTest : AnnotationSpec() {
     @Test
@@ -56,6 +58,65 @@ internal class WhitelistClassloaderTest : AnnotationSpec() {
         )
         wlc.loadClass(WhitelistClassloaderTestDummyClass::class.java.name) shouldBe WhitelistClassloaderTestDummyClass::class.java
     }
+
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - no whitelist and softMode off`() {
+        val wlc = WhitelistClassloader(false)
+        shouldThrow<BlockingClassloader.ResourceBlockedException> {
+            wlc.getResource("notOnWhitelist")
+        }
+    }
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - resource on whitelist and softMode off`() {
+        val wlc = WhitelistClassloader(false, "whitelistedResource", parent = WhitelistClassloaderTestDummyLoader())
+        wlc.getResource("whitelistedResource") shouldBe dummyUrl
+    }
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - package on whitelist and softMode off`() {
+        val wlc = WhitelistClassloader(false, "whitelisted", parent = WhitelistClassloaderTestDummyLoader())
+        wlc.getResource("whitelisted.Resource") shouldBe dummyUrl
+    }
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - no whitelist and softMode on`() {
+        val wlc = WhitelistClassloader(true)
+        wlc.getResource("notOnWhitelist") shouldBe null
+    }
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - resource on whitelist and softMode on`() {
+        val wlc = WhitelistClassloader(true, "whitelistedResource", parent = WhitelistClassloaderTestDummyLoader())
+        wlc.getResource("whitelistedResource") shouldBe dummyUrl
+    }
+
+    @Test
+    fun `FUNCTION getResource(String?) URL? - package on whitelist and softMode on`() {
+        val wlc = WhitelistClassloader(true, "whitelisted", parent = WhitelistClassloaderTestDummyLoader())
+        wlc.getResource("whitelisted.Resource") shouldBe dummyUrl
+    }
 }
 
 private class WhitelistClassloaderTestDummyClass
+
+private val dummyUrl = URL("https://i.am.mocked")
+
+private class WhitelistClassloaderTestDummyLoader : ClassLoader() {
+
+    override fun loadClass(name: String?): Class<*> = WhitelistClassloaderTestDummyClass()::class.java
+    override fun getResource(name: String?): URL? = URL("https://i.am.mocked")
+    override fun getResources(name: String?): Enumeration<URL> = WhitelistClassloaderTestDummyEnumeration()
+}
+
+private class WhitelistClassloaderTestDummyEnumeration : Enumeration<URL> {
+
+    override fun hasMoreElements(): Boolean {
+        return true
+    }
+
+    override fun nextElement(): URL {
+        return URL("https://we.love.unicorns")
+    }
+}
