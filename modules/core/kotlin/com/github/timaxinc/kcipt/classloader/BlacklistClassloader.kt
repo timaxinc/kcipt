a77086f3596202192664209fa9d2193d526248a5
mavolin <48887425+mavolin@users.noreply.github.com>
2019-10-17 19:32:14 +0200

    refactor: move Exception to superclass BlockingClassloader

--- modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlacklistClassloader.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlacklistClassloader.kt
@@ -14,7 +14,9 @@ import java.util.*
  *          the blacklist containing names of Classes, resources, as well as of packages, which may not be used when
  *          loading a class or resource.
  */
-class BlacklistClassloader(private val blacklist: List<String>, parent: ClassLoader? = null) : ClassLoader(parent) {
+class BlacklistClassloader(private val blacklist: List<String>, parent: ClassLoader? = null) : BlockingClassloader(
+        parent
+) {
 
     /**
      * Creates a BlockingClassloader with the specified parent and a blacklist containing the passed elements.
@@ -135,48 +137,4 @@ class BlacklistClassloader(private val blacklist: List<String>, parent: ClassLoa
         object Exact : Block()
         class Package(val packageName: String) : Block()
     }
-
-    /**
-     * Abstract representation of an exception thrown, if the blacklist has prevented the requested operation.
-     *
-     * @constructor
-     *          Instantiates a new BlockedException.
-     *
-     * @param message
-     *          The message of the exception
-     */
-    abstract class BlockedException internal constructor(message: String) : Exception(message)
-
-    /**
-     * ClassBlockedException gets thrown in case the specified class is present on the blacklist.
-     *
-     * @constructor
-     *          Instantiates a new ClassBlockedException.
-     *
-     * @param name
-     *          The name of the Class being blocked.
-     */
-    class ClassBlockedException internal constructor(name: String) : BlockedException("Class $name is blocked")
-
-    /**
-     * ResourceBlockedException gets thrown in case the specified resource is present on the blacklist.
-     *
-     * @constructor
-     *          Instantiates a new ResourceBlockedException.
-     *
-     * @param name
-     *          The name of the Resource being blocked.
-     */
-    class ResourceBlockedException internal constructor(name: String) : BlockedException("Resource $name is blocked")
-
-    /**
-     * PackageBlockedException gets thrown in case the package containing the class is present on the blacklist.
-     *
-     * @constructor
-     *          Instantiates a new PackageBlockedException.
-     *
-     * @param name
-     *          The name of the package causing the class to be blocked.
-     */
-    class PackageBlockedException internal constructor(name: String) : BlockedException("Package $name is blocked")
 }
--- modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlockingClassloader.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/classloader/BlockingClassloader.kt
@@ -1,3 +1,47 @@
 package com.github.timaxinc.kcipt.classloader
 
-abstract class BlockingClassloader(parent: ClassLoader?) : ClassLoader(parent)
+abstract class BlockingClassloader(parent: ClassLoader?) : ClassLoader(parent) {
+    /**
+     * Abstract representation of an exception thrown, if the blacklist has prevented the requested operation.
+     *
+     * @constructor
+     *          Instantiates a new BlockedException.
+     *
+     * @param message
+     *          The message of the exception
+     */
+    abstract class BlockedException internal constructor(message: String) : Exception(message)
+
+    /**
+     * ClassBlockedException gets thrown in case the specified class is present on the blacklist.
+     *
+     * @constructor
+     *          Instantiates a new ClassBlockedException.
+     *
+     * @param name
+     *          The name of the Class being blocked.
+     */
+    class ClassBlockedException internal constructor(name: String) : BlockedException("Class $name is blocked")
+
+    /**
+     * ResourceBlockedException gets thrown in case the specified resource is present on the blacklist.
+     *
+     * @constructor
+     *          Instantiates a new ResourceBlockedException.
+     *
+     * @param name
+     *          The name of the Resource being blocked.
+     */
+    class ResourceBlockedException internal constructor(name: String) : BlockedException("Resource $name is blocked")
+
+    /**
+     * PackageBlockedException gets thrown in case the package containing the class is present on the blacklist.
+     *
+     * @constructor
+     *          Instantiates a new PackageBlockedException.
+     *
+     * @param name
+     *          The name of the package causing the class to be blocked.
+     */
+    class PackageBlockedException internal constructor(name: String) : BlockedException("Package $name is blocked")
+}
