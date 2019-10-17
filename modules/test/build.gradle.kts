0ff484f62f737f2ef6fa79d2941a89d5da453eb0
TIMON-WORKSTATI\timon <timon.schelling@outlook.de>
2019-10-17 22:55:14 +0200

    fix: gradle dependencies

--- modules/test/build.gradle.kts
+++ modules/test/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     compile(project(":core"))
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+    implementation("io.mockk:mockk:1.9.3")
+    implementation("com.google.jimfs:jimfs:1.1")
+    implementation("com.google.guava:guava:28.1-jre")
 }
 
 tasks.test {
