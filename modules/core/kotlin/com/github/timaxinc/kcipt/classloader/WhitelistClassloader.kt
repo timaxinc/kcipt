3788acd615895d9af799a0ac36e22979d9205cae
mavolin <48887425+mavolin@users.noreply.github.com>
2019-10-17 21:36:06 +0200

    feat(WhitelistClassloader): loadClass(String?): Class?

--- modules/core/kotlin/com/github/timaxinc/kcipt/classloader/WhitelistClassloader.kt
+++ modules/core/kotlin/com/github/timaxinc/kcipt/classloader/WhitelistClassloader.kt
@@ -1,5 +1,8 @@
 package com.github.timaxinc.kcipt.classloader
 
+import com.github.timaxinc.kcipt.util.io.Block
+import com.github.timaxinc.kcipt.util.io.startsWithMember
+
 /**
  * WhitelistClassloader is a ClassLoader with a predefined Whitelist of Classes and packages that it will allow to
  * load resources and Classes from. All other requests will be blocked
@@ -33,4 +36,34 @@ class WhitelistClassloader(
     constructor(softMode: Boolean, vararg whitelist: String, parent: ClassLoader? = null) : this(
             softMode, whitelist.toList(), parent
     )
+
+    /**
+     * LoadClass loads the Class with the specified name, if the class is on the whitelist. If not and softMode is
+     * turned on, null will be returned. If softMode is off an ClassBlockedException will be thrown.
+     *
+     * @throws BlockingClassloader.ClassBlockedException
+     *          Thrown if softMode is off and the requested Class is not on the whitelist.
+     *
+     * @param name
+     *          the name of the Class that is to be loaded
+     * @return
+     *          the requested Class or null if the requested Class is not on the whitelist and softMode is on
+     */
+    override fun loadClass(name: String?): Class<*>? {
+        if (name == null) {
+            return super.loadClass(name)
+        }
+
+        return if (softMode) {
+            when (name startsWithMember whitelist) {
+                Block.None -> null
+                else       -> super.loadClass(name)
+            }
+        } else {
+            when (name startsWithMember whitelist) {
+                Block.None -> throw ClassBlockedException(name)
+                else       -> super.loadClass(name)
+            }
+        }
+    }
 }
